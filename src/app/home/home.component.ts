e2b761acdd50a0f29deac498f014a9a93f25bbb7
Octávio Bueno <ohbueno@gmail.com>
2017-06-11 14:17:05 -0300

    novo projeto

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'home',
+  templateUrl: 'home.template.html'
+})
+export class HomeComponent {
+  
+}
